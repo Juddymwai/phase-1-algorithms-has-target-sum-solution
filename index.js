735e9cf2db97facc4b0c72d5487fa7e86fa1c9cf
Judy Mwai <juddiemukami@gmail.com>
2022-06-20 00:52:35 +0300

    commit tree ad5fe76344f3c8555d55f5da41d4ac725bdc06e9

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const presentItem= new Set(); 
+  for (const item of array) {
+    const difference = target - item;
+
+    if (presentItem.has(difference)) return true;
+
+  
+    presentItem.add(item);
+  }
+  return false;
 }
 
 /* 
@@ -8,10 +18,21 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  define an empty array
+  use for of to iterate the array
+  take the diiference beteween the item being iterated and the target
+  use if to check comparison between the difference and an item in the array
+  if the difference matches the item in the array return true, otherwise return false
+  the itemshould be added to the the set after the if condition
 */
 
 /*
   Add written explanation of your solution here
+  the code first define a new empty set that would hold result afterwars
+  the items of the array are iterated by use oof for of
+  the code assign a variable to the differenece between an item in the array and the target
+  it then uses if condition to check for comparison
+  when the comparison is met, a true result is returned
 */
 
 // You can run `node index.js` to view these console logs
